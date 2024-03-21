a87fdf445fe3cf6df5ff92ad34a7d7058ec208b2
Kashish Gupta <guptakashishiit40@gmail.com>
2024-03-21 15:37:39 +0530

    save

--- task1.js
+++ task1.js
@@ -1,27 +1,37 @@
-function ValidatePassword() {
-
-    var password = prompt("Please enter password to proceed", "");
-    
-    if (password == "Admin Password") {
+function validatePassword() {
+    // Use const for variables that won't be reassigned
+    const password = prompt("Please enter password to proceed", "");
+    if (password === "Admin Password") {
       return true;
     }
-    return false;}
-    var showDivLength = function () {
-        var div =  document.getElementsByTagName('div');
-        divLength = div.length;
-        if (divLength > 0)
-            div[divLength-1].innerHTML = divLength;
-        return divLength;
+    return false;
+  }
+  
+  function showDivLength() {
+    const divs = document.getElementsByTagName("div");
+    const divLength = divs.length;
+    if (divLength > 0) {
+      divs[divLength - 1].innerHTML = divLength;
     }
-    
-    function testJS() {
-        // Use const for variables that won't be reassigned
-        const inputElement = document.getElementById('name'),
-              // Add default value for the query parameter
-              baseUrl = 'task1.html',
-              queryParameter = inputElement.value || '';
-        // Use template literals for string concatenation
-        const url = `${baseUrl}?q=${encodeURIComponent(queryParameter)}`;
-        // Use window.location.href instead of document.location.href
-        window.location.href = url;
-    }
+    return divLength;
+  }
+  
+  function testJS() {
+    // Use const for variables that won't be reassigned
+    const inputElement = document.getElementById("name");
+    const baseUrl = "task1.html";
+    const queryParameter = inputElement.value || "";
+  
+    // Use template literals for string concatenation
+    const url = `${baseUrl}?q=${encodeURIComponent(queryParameter)}`;
+  
+    // Use window.location.href instead of document.location.href
+    window.location.href = url;
+  }
+  
+  // Call the functions when the page loads
+  if (validatePassword()) {
+    const divLength = showDivLength();
+    console.log(`There are ${divLength} div elements on the page.`);
+    testJS();
+  }
